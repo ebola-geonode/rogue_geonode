fced629e7c665e47cfc43861a6a91cf2187246ba
Jeffrey Johnson <ortelius@gmail.com>
2014-11-06 07:56:22 -0800

    Add site_index.html to urls.py

--- geoshape/urls.py
+++ geoshape/urls.py
@@ -5,6 +5,7 @@ from maploom.geonode.urls import urlpatterns as maploom_urls
 urlpatterns = patterns('',
                        (r'^file-service/', include('geoshape.file_service.urls')),
                        (r'^proxy/', 'geoshape.views.proxy'),
+                       (r'^$', 'geonode.views.index', {'template': 'site_index.html'}, name='home'),
                        )
 
 urlpatterns += geonode_url_patterns
